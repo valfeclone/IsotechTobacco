e5d1b8d06c9f3562d6e9f30b8bb25354e26f0dae
fadhlan <fadhlanpasyah13@gmail.com>
2022-02-06 12:10:04 +0700

    cart inheritance

--- IsotechTobacco/app/Models/User.php
+++ IsotechTobacco/app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Order::class);
     }
+
+    public function cart()
+    {
+        return $this->hasMany(Cart::class);
+    }
 }
