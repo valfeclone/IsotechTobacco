72dd2b3d2c48bf79b3e48187a4ca318ab0694e54
dharmatriyasa <dharmatriyasa19@gmail.com>
2022-05-22 02:29:28 +0800

    fix: merged with commit badhlan

--- IsotechTobacco/config/snappy.php
+++ IsotechTobacco/config/snappy.php
@@ -1,69 +1,69 @@
 <?php
 
-return [
+// return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Snappy PDF / Image Configuration
-    |--------------------------------------------------------------------------
-    |
-    | This option contains settings for PDF generation.
-    |
-    | Enabled:
-    |    
-    |    Whether to load PDF / Image generation.
-    |
-    | Binary:
-    |    
-    |    The file path of the wkhtmltopdf / wkhtmltoimage executable.
-    |
-    | Timout:
-    |    
-    |    The amount of time to wait (in seconds) before PDF / Image generation is stopped.
-    |    Setting this to false disables the timeout (unlimited processing time).
-    |
-    | Options:
-    |
-    |    The wkhtmltopdf command options. These are passed directly to wkhtmltopdf.
-    |    See https://wkhtmltopdf.org/usage/wkhtmltopdf.txt for all options.
-    |
-    | Env:
-    |
-    |    The environment variables to set while running the wkhtmltopdf process.
-    |
-    */
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Snappy PDF / Image Configuration
+//     |--------------------------------------------------------------------------
+//     |
+//     | This option contains settings for PDF generation.
+//     |
+//     | Enabled:
+//     |    
+//     |    Whether to load PDF / Image generation.
+//     |
+//     | Binary:
+//     |    
+//     |    The file path of the wkhtmltopdf / wkhtmltoimage executable.
+//     |
+//     | Timout:
+//     |    
+//     |    The amount of time to wait (in seconds) before PDF / Image generation is stopped.
+//     |    Setting this to false disables the timeout (unlimited processing time).
+//     |
+//     | Options:
+//     |
+//     |    The wkhtmltopdf command options. These are passed directly to wkhtmltopdf.
+//     |    See https://wkhtmltopdf.org/usage/wkhtmltopdf.txt for all options.
+//     |
+//     | Env:
+//     |
+//     |    The environment variables to set while running the wkhtmltopdf process.
+//     |
+//     */
     
-    'pdf' => [
-        'enabled' => true,
-        'binary'  => env('WKHTML_PDF_BINARY', '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"'),
-        'timeout' => false,
-        'options' => [],
-        'env'     => [],
-    ],
+//     'pdf' => [
+//         'enabled' => true,
+//         'binary'  => env('WKHTML_PDF_BINARY', '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"'),
+//         'timeout' => false,
+//         'options' => [],
+//         'env'     => [],
+//     ],
     
-    'image' => [
+//     'image' => [
+//         'enabled' => true,
+//         'binary'  => env('WKHTML_IMG_BINARY', '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"'),
+//         'timeout' => false,
+//         'options' => [],
+//         'env'     => [],
+//     ],
+
+// ];
+
+return array(
+    'pdf' => array(
         'enabled' => true,
-        'binary'  => env('WKHTML_IMG_BINARY', '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"'),
+        'binary' => '"D:\wkhtml\wkhtmltopdf\bin\wkhtmltopdf.exe"',
+        'options' => array(),
         'timeout' => false,
-        'options' => [],
-        'env'     => [],
-    ],
 
-];
+    ),
+    'image' => array(
+        'enabled' => true,
+        'binary' => '"D:\wkhtml\wkhtmltopdf\bin\wkhtmltoimage.exe"',
+        'options' => array(),
+        'timeout' => false,
 
-// return array(
-//     'pdf' => array(
-//         'enabled' => true,
-//         'binary' => '"D:\wkhtml\wkhtmltopdf\bin\wkhtmltopdf.exe"',
-//         'options' => array(),
-//         'timeout' => false,
-
-//     ),
-//     'image' => array(
-//         'enabled' => true,
-//         'binary' => '"D:\wkhtml\wkhtmltopdf\bin\wkhtmltoimage.exe"',
-//         'options' => array(),
-//         'timeout' => false,
-
-//     ),
-// );
+    ),
+);
