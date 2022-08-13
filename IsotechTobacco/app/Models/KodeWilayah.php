dfbdce13c054c5aef2fe9fe6bcc3e4794e996334
fadhlan <fadhlanpasyah13@gmail.com>
2022-08-13 17:28:26 +0700

    added location database

---
+++ IsotechTobacco/app/Models/KodeWilayah.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KodeWilayah extends Model
+{
+    use HasFactory;
+                /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'KODE PROVINSI',
+        'PROVINSI',
+        'KODE KAB/KOTA',
+        'KAB/KOTA',
+        'KODE KEC',
+        'KECAMATAN',
+        'KODE KEL/DESA',
+        'KELURAHAN/DESA',
+        'KODE POS',
+        'PROVINSI JNT',   
+        'KAB/KOTA JNT',  
+        'KECAMATAN JNT',   
+        'KODE',   
+        'KODE KOTA JNT',   
+        'Note'   
+    ];
+}
---
+++ IsotechTobacco/database/migrations/2022_08_12_083747_create_kodewilayah_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKodewilayahTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kodewilayah', function (Blueprint $table) {
+            $table->id();
+            $table->string("KODE_PROVINSI");
+            $table->string("PROVINSI");
+            $table->string("KODE_KAB_KOTA");
+            $table->string("KAB_KOTA");
+            $table->string("KODE_KEC");
+            $table->string("KECAMATAN");
+            $table->string("KODE_KEL_DESA");
+            $table->string("KELURAHAN_DESA");
+            $table->string("KODE_POS");
+            $table->string("PROVINSI_JNT");
+            $table->string("KAB_KOTA_JNT");
+            $table->string("KECAMATAN_JNT");
+            $table->string("KODE");
+            $table->string("KODE_KOTA_JNT");
+            $table->string("Note")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kodewilayah');
+    }
+}
